144b6f147629556e09d5a228b386f49a17d9d1ff
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-21 12:27:01 +0300

    Final version
    Signed-off-by: Ion Varsescu <19718355+Nei-V@users.noreply.github.com>

---
+++ Lessons learned.md
@@ -0,0 +1,6 @@
+1. Function to create objects
+2. At this stage, it is easier for me to create many variables to understand the logic, then remove them one by one.
+For example, at first I had variables for firstName, lastName, fullName, then I replaced them with items in a single array fullNameArray[0] and fullNameArray[1] and fullNameArray.join(" "). The same can be done in the functions on the buttons, like in the setFullName button. That should be 2 lines less of code (for each button),I'm not sure that is has any impact on performance.
+3. In HTML you shouldn't set an ID with the exact same name as the function - I don't know exactly why, but I got errors (that is why the functions on the buttons have "1" at the end). Also, the functions should have different names. Quite obvious now, but at first I had used the excat same name for the functions on the buttons and the functions in the Person functions, which, of course, didn't work.
+4. Using split and join methods
+5. Scope, Closures, Obhects ... In this github exercise I've created a new object, "yosi" to emphasize that the objects have different scopes, so the fullNameArray variable is different for each of them.
--- makeaperson.js
+++ makeaperson.js
@@ -3,50 +3,76 @@
 var Person = function (firstAndLast) {
     // Complete the method below and implement the others similarly
 
-   let fullName;
-    
-    this.setFirstName = function (first) {
 
+    let fullNameArray = firstAndLast.split(" ");
+
+    this.setFirstName = function (first) {
+        fullNameArray[0] = first;
     };
     this.setLastName = function (last) {
-
+        fullNameArray[1] = last;
     };
     this.setFullName = function (firstAndLast) {
         console.log(firstAndLast);
-        fullName = firstAndLast;
-        console.log(fullName);
-        };
-
+        fullNameArray = firstAndLast.split(" ");
+        console.log(fullNameArray);
+    };
     this.getFullName = function () {
-       
-        return fullName;
+        return fullNameArray.join(" ");
     };
     this.getFirstName = function () {
-        return "";
+        return fullNameArray[0];
     };
     this.getLastName = function () {
-        return "";
+        return fullNameArray[1];
     };
-    
-    return firstAndLast;
-    
+
+    //return firstAndLast;
+
 };
 
 var bob = new Person('Bob Ross');
+var yosi = new Person('yosi mosi');
 //bob.getFullName();
 console.log(bob.getFullName());
+console.log(bob);
+console.log(yosi.getFullName());
+console.log(Person);
 
 function setFullNameButton1() {
-    let fullNameInput = document.getElementById("nameField").value;
-    console.log(fullNameInput);
-    bob.setFullName(fullNameInput);
+    //let fullNameInput = document.getElementById("nameField").value;
+    //console.log(fullNameInput);
+    bob.setFullName(document.getElementById("nameField").value);
     //document.getElementById("result").innerHTML = "test1";
-    
-
+};
+function setFirstNameButton1() {
+    let firstNameInput = document.getElementById("nameField").value;
+    console.log(firstNameInput);
+    bob.setFirstName(firstNameInput);
+    //document.getElementById("result").innerHTML = "test1";
+};
+function setLastNameButton1() {
+    let lastNameInput = document.getElementById("nameField").value;
+    console.log(lastNameInput);
+    bob.setLastName(lastNameInput);
+    //document.getElementById("result").innerHTML = "test1";
 };
 
 function getFullNameButton1() {
     document.getElementById("result").innerHTML = bob.getFullName();
     console.log(Object.keys(bob).length);
+    console.log(bob.getFullName());
+console.log(yosi.getFullName());
 };
 
+function getFirstNameButton1() {
+    document.getElementById("result").innerHTML = bob.getFirstName();
+    console.log(Object.keys(bob).length);
+};
+
+function getLastNameButton1() {
+    document.getElementById("result").innerHTML = bob.getLastName();
+    console.log(Object.keys(bob).length);
+};
+console.log(bob);
+console.log(yosi);
